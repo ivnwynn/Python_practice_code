4011c9cd916967977251cf1280231b1397b8ded3
ivnwynn <sanjuanivan0522@gmail.com>
2026-01-27 21:58:02 +0800

    greetings.py

---
+++ greetings.py
@@ -0,0 +1,8 @@
+def greetings():
+    name = input("Enter your name please: ")
+    if name == "ivan":
+        print("Welcome! How may I help you today sir?")
+    else:
+        print("Unauthorized Personel is Prohibited")
+
+greetings()
